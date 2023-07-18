b2f2adfe4880eca83d1930ddac78975c6ca21d9b
carlose <cjsaltlake@gmail.com>
2023-07-18 11:59:47 -0400

    Update

--- utils/data.py
+++ utils/data.py
@@ -57,12 +57,13 @@ def eval_model(model, data_loader, name):
 
 
 def log_every_n_percent(epoch, batch_idx, data_loader, percent):
-    pct_complete = 100.0 * (batch_idx + 1) / len(data_loader)
-    if pct_complete % percent == 0:
+    log_ids = set(int(i * len(data_loader) / 100) for i in range(0, 101, percent))
+    if batch_idx in log_ids:
         complete = (batch_idx + 1)
         total = len(data_loader.dataset)
+        pct_complete = 100.0 * (batch_idx + 1) / len(data_loader)
         print(f'Epoch {epoch}: {complete}/{total} ({pct_complete:.0f}%)')
-
+        
 
 def log_every_n_steps(epoch, batch_idx, data_loader, steps):
     if (batch_idx + 1) % steps == 0:
